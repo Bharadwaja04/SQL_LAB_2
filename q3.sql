15ce220e9a14e3cd7b95f1f652539c5283211dc5
Bharadwaj <114684643+Bharadwaja04@users.noreply.github.com>
2025-08-16 19:42:44 +0530

    Update q3.sql

--- q3.sql
+++ q3.sql
@@ -4,9 +4,18 @@ CREATE PROCEDURE AddSubscriberIfNotExists(
 	IN subName varchar(100)
 )
 BEGIN 
+	DECLARE ID INT;
 	IF NOT EXISTS (Select SubscriberName from Subscribers where SubscriberName = subName) THEN
-    INSERT INTO Subscribers (SubscriberName, SubscriptionDate)
-    VALUES (subName, CURDATE());
+		SELECT MAX(SubscriberID) INTO ID FROM Subscribers;
+		
+        IF ID IS NULL THEN
+			SET ID = 1;
+		ELSE
+			SET ID = ID + 1;
+		END IF;
+        
+		INSERT INTO Subscribers (SubscriberID, SubscriberName, SubscriptionDate)
+		VALUES (ID,subName, CURDATE());
     ELSE
 		SELECT CONCAT(subName, ' already exists') AS MESSAGE;
 	END IF;
@@ -14,4 +23,5 @@ END //
 
 DELIMITER ;
 
-CALL AddSubscriberIfNotExists('John Doe');
+
+
